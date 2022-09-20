b44109ffefb1b8f15c88553c8272269045cc214a
polyqubit <polyumbrial@gmail.com>
2022-09-20 14:17:41 -0700

    fix?

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -52,7 +52,7 @@ int main(int, char**)
     const char* glsl_version = "#version 460";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
