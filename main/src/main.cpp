50d9688da5128a5ce633184dbdc7af3d72d816a8
polyqubit <polyumbrial@gmail.com>
2022-10-30 01:24:55 -0700

    texture renders!!
    unfortunately not transparent

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -183,10 +183,10 @@ int main(int, char**)
         // positions          // texture coords
          0.5f,  0.5f, 0.0f,   1.0f, 1.0f,   // top right
          0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f,   // top left 
 
         -0.5f,  0.5f, 0.0f,   0.0f, 1.0f,   // top left 
-         0.5f,  0.5f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
         -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,   // bottom left
     };
 
@@ -199,9 +199,9 @@ int main(int, char**)
     glBindVertexArray(VAOIds[0]);
     glBindBuffer(GL_ARRAY_BUFFER, VBOIds[0]);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(6 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     glGenFramebuffers(1, &FBOIds[0]);
